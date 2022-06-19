f992212a4defc6ee40cfbaf24f12fe88028e4930
Ava-yy <55901577+Ava-yy@users.noreply.github.com>
2022-06-18 23:57:00 -0500

    Add files via upload

---
+++ datasets_prep/__init__.py
@@ -0,0 +1,35 @@
+from .imagenet_dataset import ImageNetDataset
+from .coco_dataset import CocoDataset
+from .imagenet_s_dataset import ImageNetSDataset
+
+import torchvision.transforms as transforms
+from PIL import Image
+
+
+def get_transform(name='image'):
+    if name == 'image':
+        return transforms.Compose([transforms.Resize(256),
+                                    transforms.CenterCrop(224),
+                                    transforms.ToTensor(),
+                                    transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225]),])
+    elif name == 'ground_truth':
+        return transforms.Compose([transforms.ToPILImage(),
+                                    transforms.Resize(256, Image.NEAREST),
+                                    transforms.CenterCrop(224),
+                                    transforms.ToTensor()])
+    elif name == 'reverse_image':
+        return transforms.Compose([transforms.Normalize(mean=[0, 0, 0], std=[4.3668, 4.4643, 4.4444]),
+                                    transforms.Normalize(mean=[-0.485, -0.456, -0.406], std=[1, 1, 1]),
+                                    transforms.ToPILImage(),])
+    else:
+        raise KeyError("transform name is invalid!")
+
+def get_dataset(name='imagenet'):
+    if name == 'imagenet':
+        return ImageNetDataset
+    elif name == 'imagenet_s':
+        return ImageNetSDataset
+    elif name == 'coco':
+        return CocoDataset
+    else:
+        raise KeyError("dataset name is invalid!")
